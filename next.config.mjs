db72ffff0c5d4d0afa4d97da0fa7b7f3589d92ee
Kohei Shingai <koheishingai@microsoft.com>
2025-01-20 06:36:10 +0530

    'test'

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "standalone",
+    env: {
+      DB_HOST: process.env.DB_HOST,
+      DB_USER: process.env.DB_USER,
+      DB_DATABASE: process.env.DB_DATABASE,
+      DB_PASSWORD: process.env.DB_PASSWORD,
+      DB_PORT: process.env.DB_PORT,
+    },
+  };
+  
 
 export default nextConfig;
